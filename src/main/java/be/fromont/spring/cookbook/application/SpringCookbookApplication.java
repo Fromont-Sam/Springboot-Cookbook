55f1816a87e68170b16c0adba635299c481eced6
Fromont Sam <fromont.sam@gmail.com>
2022-11-09 12:04:40 +0100

    First commit

---
+++ src/main/java/be/fromont/spring/cookbook/application/SpringCookbookApplication.java
@@ -0,0 +1,13 @@
+package be.fromont.spring.cookbook.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCookbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/fromont/spring/cookbook/application/SpringCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package be.fromont.spring.cookbook.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
